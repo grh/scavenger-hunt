284f497fd036661a9287f25285d852f096e6f9f8
Guymon Hall <guymon.hall@guymonhall.CS.unlv.edu>
2016-04-29 16:05:27 -0700

    wrote rake documentation task

--- sample-doc.rb
+++ sample-doc.rb
@@ -1,31 +1,26 @@
 =begin
-= ABOUT -> h3
+= ABOUT
 
-    -> p
     Welcome to UNLV's QR Code Scavenger Hunt Site! This program was originally
     conceived by Ed Jorgensen and Guymon Hall to help students familiarize
     themselves with various locations around the UNLV campus.
 
-= CONTRIBUTORS -> h3
+= CONTRIBUTORS
 
-    -> ul
-    * Reiley Porter -> li
-    * Carmon Wong -> li
-    * Beverly Wood -> li
+    * Reiley Porter
+    * Carmon Wong
+    * Beverly Wood
+    * Boaty McBoatface
 
-    -> p
     We encourage you to contribute to this project by contacting Guymon for
-    more information on how to get started., 
+    more information on how to get started.
 
-= LICENSE -> h3
+= LICENSE
 
-    -> p
     This application and its source code are licensed under the MIT License:
 
-    -> p
     Copyright (c) 2016 Guymon Hall
 
-    -> p
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the
     "Software"), to deal in the Software without restriction, including
@@ -34,11 +29,9 @@
     permit persons to whom the Software is furnished to do so, subject to
     the following conditions:
 
-    -> p
     The above copyright notice and this permission notice shall be
     included in all copies or substantial portions of the Software.
 
-    -> p
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -46,6 +39,7 @@
     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 =end
 
 =begin
